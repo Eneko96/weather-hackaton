faabb5ebbf3898b72edb6b3e5ab7dea1d9c7d489
Eneko96 <imr.aem2526@gmail.com>
2022-05-26 18:03:18 +0200

    refactor and added max min on header

---
+++ src/components/Header.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { kinds } from '../services/weatherKinds';
+	import { fade } from 'svelte/transition';
+	import WeatherIcon from '../components/weather-icon.svelte';
+	export let locationName;
+	export let temperature;
+	export let isDay;
+	export let conditionText;
+	export let range;
+	export let { maxTemp, minTemp } = range;
+</script>
+
+<h1>{locationName}</h1>
+<section class="first-box" transition:fade>
+	<div>
+		<h2>{temperature}º</h2>
+	</div>
+	<div class="condition">
+		<WeatherIcon icon={kinds[isDay ? 'day' : 'night'].partlyCloudy} />
+		<div>{maxTemp}º/{minTemp}º</div>
+	</div>
+	<h3>{conditionText}</h3>
+</section>
+
+<style>
+	.first-box {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.condition {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		align-self: baseline;
+	}
+
+	h2 {
+		font-weight: 300;
+		font-size: 120px;
+		color: #333;
+		text-transform: uppercase;
+		padding: 0;
+	}
+
+	h3 {
+		position: relative;
+		align-self: baseline;
+		left: auto;
+		font-weight: 700;
+		transform: rotate(-90deg);
+		font-size: 10px;
+		text-align: left;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,22 @@
 <script>
-	import { getWeatherFrom, getForecast } from '../services/weather.js';
+	import { getForecast } from '../services/weather.js';
 	import WeatherInfo from '../components/weather-footer.svelte';
-	import WeatherIcon from '../components/weather-icon.svelte';
 	import Forecast from '../components/Forecast.svelte';
 	import Searcher from '../components/Searcher.svelte';
-	import { kinds } from '../services/weatherKinds.js';
-	import { fade } from 'svelte/transition';
+	import Header from '../components/Header.svelte';
 	let coords = [];
 	let firstSearch = false;
-	let weatherKey = 86;
 	let forecast = null;
 	const handleForecast = async (value) => {
 		forecast = await getForecast(`${value}&days=5`);
 		console.log(forecast);
 	};
 
-	let weather = null;
 	$: if (typeof window !== 'undefined' && !firstSearch) {
 		const { geolocation } = navigator;
 		geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
 			coords = [latitude, longitude];
 			const getData = async () => {
-				weather = await getWeatherFrom(coords);
 				handleForecast(`${coords}&days=5`);
 			};
 			getData();
@@ -31,29 +26,28 @@
 
 	const handleSearch = async (value) => {
 		coords = [];
-		weather = await getWeatherFrom(value);
 		forecast = await getForecast(`${value}&days=5`);
 	};
 </script>
 
 <main>
-	{#if weather !== null}
-		{#key weatherKey}
-			<h1>{weather.locationName}</h1>
-			<section class="first-box" transition:fade>
-				<div>
-					<h2>{weather.temperature}º</h2>
-				</div>
-				<div class="condition">
-					<WeatherIcon
-						text={weather.conditionText}
-						icon={kinds[weather.isDay ? 'day' : 'night'].partlyCloudy}
-					/>
-					<div>max/min</div>
-				</div>
-				<h3>{weather.conditionText}</h3>
-			</section>
-			<WeatherInfo humidity={weather.humidity} feel={weather.feelsLike} wind={weather.windSpeed} />
+	{#if forecast !== null}
+		{#key forecast.locationName}
+			<Header
+				conditionText={forecast.conditionText}
+				locationName={forecast.locationName}
+				isDay={forecast.isDay}
+				temperature={forecast.temperature}
+				range={{
+					maxTemp: forecast.forecast[0].day.maxtemp_c,
+					minTemp: forecast.forecast[0].day.mintemp_c
+				}}
+			/>
+			<WeatherInfo
+				humidity={forecast.humidity}
+				feel={forecast.feelsLike}
+				wind={forecast.windSpeed}
+			/>
 		{/key}
 	{:else}
 		<div class="loading-container">
@@ -85,41 +79,12 @@
 		padding: 2rem;
 		height: 100%;
 	}
-	.first-box {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.condition {
-		align-items: center;
-		display: flex;
-		flex-direction: column;
-		align-self: baseline;
-	}
 
 	h1 {
 		font-weight: 300;
 		color: #333;
 		text-transform: uppercase;
 		padding: 16px 0 0 0;
-	}
-
-	h2 {
-		font-weight: 300;
-		font-size: 120px;
-		color: #333;
-		text-transform: uppercase;
-		padding: 0;
-	}
-
-	h3 {
-		position: relative;
-		align-self: baseline;
-		left: auto;
-		font-weight: 700;
-		transform: rotate(-90deg);
-		font-size: 10px;
-		text-align: left;
 	}
 
 	ul {
